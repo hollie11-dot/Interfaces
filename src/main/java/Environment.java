33e5d523fd444a958eb68f598326821d6acbc264
Hollie <holliemount11@hotmail.com>
2024-10-23 12:20:28 +0100

    feat: add vehicles atrribute and checkVehicles method

--- src/main/java/Environment.java
+++ src/main/java/Environment.java
@@ -4,11 +4,19 @@ import java.util.List;
 public abstract class Environment<T> {
 
     protected List<T> traffic = new ArrayList<>();
+    protected List<T> vehicles = new ArrayList<>();
 
     public abstract void checkTraffic();
 
     public void addTraffic(T traffic) {
+        if (traffic instanceof Vehicle) {
+            this.vehicles.add(traffic);
+        }
+
         this.traffic.add(traffic);
     }
+
+    public abstract void checkVehicles();
+
 
 }
