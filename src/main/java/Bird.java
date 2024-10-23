7562269c55d4ea1f6686d1d704d0c37c64625a14
Hollie <holliemount11@hotmail.com>
2024-10-23 10:52:31 +0100

    feat: add fly method to Bird class

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -1,2 +1,5 @@
-public abstract class Bird extends Animal {
+public abstract class Bird extends Animal implements Flyable {
+    public void fly(){
+        System.out.println(this.getClass().getSimpleName() + " is flying!");
+    }
 }
