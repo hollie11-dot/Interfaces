74fb03ada65e23b4fdb66e3138031cfb790573b1
yee0802 <NovusAa12@proton.me>
2024-10-23 11:49:06 +0100

    feat: add functionality tests to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        /* // Test 1
         List<Object> objects = List.of(
                 new Aeroplane(),
                 new Car(),
@@ -25,5 +26,22 @@ public class Main {
             }
 
         }
+        */
+
+        Sky sky = new Sky();
+        sky.addTraffic(new Duck());
+        sky.addTraffic(new Pigeon());
+        sky.addTraffic(new Aeroplane());
+
+        Water water = new Water();
+        water.addTraffic(new Duck());
+        water.addTraffic(new Fish());
+
+        List<Environment<?>> environments = List.of(sky, water);
+
+        for (Environment<?> environment: environments) {
+            environment.checkTraffic();
+        }
+
     }
 }
